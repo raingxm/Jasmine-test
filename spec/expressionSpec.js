06b88886be1174c5a4cb29cad56358ad5fe713cb
raingxm <raingxm@163.com>
2015-07-13 21:18:59 +0800

    rename file and fixed js warning

--- spec/expressionSpec.js
+++ spec/expressionSpec.js
@@ -1,11 +1,11 @@
 describe("add function work properly", function() {
     it("add two positive number work fine", function() {
         expect(add(3, 5)).toBe(8);
-    })
+    });
 });
 
 describe("minus function work properly", function() {
     it("two number minus need work fine", function() {
         expect(minus(2, 10)).toBe(-8);
-    })
+    });
 });
--- src/expression.js
+++ src/expression.js
@@ -1,7 +1,7 @@
 var add = function(a, b) {
     return a + b;
-}
+};
 
 var minus = function(a, b) {
     return a - b;
-}
+};
