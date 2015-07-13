127c8449afccb1abfe6c9629a279cb2e3602ac12
raingxm <raingxm@163.com>
2015-07-13 21:14:43 +0800

    add jshint to check js file

--- my.conf.js
+++ my.conf.js
@@ -28,7 +28,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        '.src/*.js': ['jshint']
+        './src/*.js': ['jshint'],
+        './spec/*.js': ['jshint']
     },
 
 
