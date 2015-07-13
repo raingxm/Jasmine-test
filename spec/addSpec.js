5239b0675b8cc54c1bb70f45dea2f52d8131df38
raingxm <raingxm@163.com>
2015-07-13 20:56:58 +0800

    add minus function

--- spec/addSpec.js
+++ spec/addSpec.js
@@ -3,3 +3,9 @@ describe("add function work properly", function() {
         expect(add(3, 5)).toBe(8);
     })
 });
+
+describe("minus function work properly", function() {
+    it("two number minus need work fine", function() {
+        expect(minus(2, 10)).toBe(-8);
+    })
+});
